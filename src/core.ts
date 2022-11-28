164c0f0de33243e0a5759532a1a0307a5085e2c0
Alejandro Castón Molero <alejandro.caston@fathom3.com>
2022-11-28 13:17:11 +0100

    cleanup unneeded type casts

--- src/core.ts
+++ src/core.ts
@@ -6,11 +6,11 @@ export type ErrResult <L> = { error: L, [IsErrSymbol]: true }
 export type Result<L, R> = ErrResult<L> | OkResult<R>
 
 export function Err <L> (error: L): ErrResult<L> {
-  return { error, [IsErrSymbol]: true } as ErrResult<L>
+  return { error, [IsErrSymbol]: true }
 }
 
 export function Ok <R> (value: R): OkResult<R> {
-  return { value, [IsErrSymbol]: false } as OkResult<R>
+  return { value, [IsErrSymbol]: false }
 }
 
 export function isErr <L, R> (result: Result<L, R>): result is ErrResult<L> {
