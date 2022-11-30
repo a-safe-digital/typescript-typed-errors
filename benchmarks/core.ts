07ecdb37bbe623225b439995ea8545248071265b
Alejandro Castón Molero <alejandro.caston@fathom3.com>
2022-11-30 11:18:24 +0100

    added benchmarks

---
+++ benchmarks/core.ts
@@ -0,0 +1,44 @@
+import b from 'benny'
+import tsResults from 'ts-results'
+import oxide from 'oxide.ts'
+import { Ok, isOk } from '../src/core.js'
+
+function Ts () {
+  return tsResults.Ok(true)
+}
+
+function Oxide () {
+  return oxide.Ok(true)
+}
+
+function TypedErrors () {
+  return Ok(true)
+}
+
+b.suite(
+  'typed error handling (ok value)',
+
+  b.add('typescript-typed-errors', () => {
+    const result = TypedErrors()
+    if (isOk(result)) {
+      return result.value
+    }
+  }),
+
+  b.add('ts-results', () => {
+    const result = Ts()
+    if (result.ok) {
+      return result.val
+    }
+  }),
+
+  b.add('oxide.ts', () => {
+    const result = Oxide()
+    if (result.isOk()) {
+      return result.unwrap()
+    }
+  }),
+
+  b.cycle(),
+  b.complete(),
+)
