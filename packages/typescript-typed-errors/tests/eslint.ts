a5886b35519fe8fdbf6e5ff785c6c1bd5b0f174b
Aex12 <alexcastonmolero@gmail.com>
2022-12-07 22:59:20 +0100

    wrap/unwrap eslint rule

--- packages/typescript-typed-errors/tests/eslint.ts
+++ packages/typescript-typed-errors/tests/eslint.ts
@@ -1,6 +1,24 @@
-import { Err, Ok } from '../src/core.js'
+import { wrap, unwrap, Ok, Err } from '../src/index.js'
 
-export function probandoEslint (n: number) {
+async function maybeError (isErr: boolean) {
+  return isErr
+    ? Err({ code: 'MaybeError' as const })
+    : Ok(true)
+}
+
+async function maybeManyErrors (err: number) {
+  if (err === 0) {
+    return Ok(true)
+  } else if (err === 1) {
+    return Err({ code: '1' })
+  } else if (err === 2) {
+    return Err({ code: '2' })
+  } else {
+    return Err({ code: '>=3' })
+  }
+}
+
+export async function probandoEslint (n: number) {
   switch (n) {
     case 1:
       return Ok(true)
@@ -9,5 +27,21 @@ export function probandoEslint (n: number) {
     case 3:
       return Ok(null)
   }
-  return undefined
+  return Ok(undefined)
 }
+
+export const wrappedFunction = wrap<typeof maybeManyErrors | typeof maybeError | typeof probandoEslint>()(async (n: number) => {
+  const value1 = unwrap(await maybeManyErrors(n))
+  const value2 = unwrap(await maybeError(true))
+  const value3 = unwrap(await probandoEslint(n))
+  if (value1 === false) {
+    return Err({ code: 'value1===false' })
+  }
+  if (value2 === false) {
+    return Err({ code: 'value2===false' })
+  }
+  if (value3 === false) {
+    return Err({ code: 'value3===false' })
+  }
+  return Ok(true)
+})
