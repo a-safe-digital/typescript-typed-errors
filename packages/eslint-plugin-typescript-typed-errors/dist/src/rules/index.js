7b858c03c465fcacebfdb5348b120a379956ef83
Aex12 <alexcastonmolero@gmail.com>
2022-12-07 20:40:35 +0100

    wip: workspaces

--- packages/eslint-plugin-typescript-typed-errors/dist/src/rules/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const consistent_result_return_js_1 = __importDefault(require("./consistent-result-return.js"));
-exports.default = {
-    'consistent-result-return': consistent_result_return_js_1.default,
-};
